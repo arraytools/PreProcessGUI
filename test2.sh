ebe21a9bc7f45c1971f4827594725badb4cc2552
arraytools <arraytools@gmail.com>
2014-07-24 16:30:35 -0400

    add test2.sh

---
+++ test2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Installation script for RNA-Seq data preprocessing
+# 
+# Note that there is a dependency issue. So getting the latest version may not work. 
+# For example, tophat v2.0.11 only supports bowtie2 v2.2.1 but not v2.2.2.
+
+SRATOOLKIT_URL=http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.3.5-2/sratoolkit.2.3.5-2-ubuntu64.tar.gz
+BOWTIE2_URL=http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.1/bowtie2-2.2.1-linux-x86_64.zip/download
+TOPHAT_URL=http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.11.Linux_x86_64.tar.gz
+SAMTOOLS_URL=http://sourceforge.net/projects/samtools/files/samtools/0.1.19/samtools-0.1.19.tar.bz2/download
+HTSEQ_URL=https://pypi.python.org/packages/source/H/HTSeq/HTSeq-0.6.1.tar.gz#md5=b7f4f38a9f4278b9b7f948d1efbc1f05
+FASTQC_URL=http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.10.1.zip
+FASTX_URL=http://hannonlab.cshl.edu/fastx_toolkit/fastx_toolkit_0.0.13_binaries_Linux_2.6_amd64.tar.bz2
+
+set -e
+# update repository
+sleep 6
