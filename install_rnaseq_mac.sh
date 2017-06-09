3f7888bd49e2678b66e2defba72350b988fe0a22
arraytools <arraytools@gmail.com>
2017-06-09 10:36:39 -0400

    fall back tophat version from 2.1.1 to 2.1.0 due to an error message segment-based junction search failed with err =-6   Reason: image not found

--- install_rnaseq_mac.sh
+++ install_rnaseq_mac.sh
@@ -8,7 +8,7 @@
 SRATOOLKIT_URL=https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.8.2-1/sratoolkit.2.8.2-1-mac64.tar.gz
 BWA_URL=https://github.com/lh3/bwa/releases/download/v0.7.15/bwa-0.7.15.tar.bz2
 BOWTIE2_URL=https://github.com/BenLangmead/bowtie2/releases/download/v2.2.9/bowtie2-2.2.9-macos-x86_64.zip
-TOPHAT_URL=http://ccb.jhu.edu/software/tophat/downloads/tophat-2.1.1.OSX_x86_64.tar.gz
+TOPHAT_URL=http://ccb.jhu.edu/software/tophat/downloads/tophat-2.1.0.OSX_x86_64.tar.gz
 STAR_URL=https://github.com/alexdobin/STAR/archive/2.5.2b.tar.gz
 SAMTOOLS_URL=https://github.com/samtools/samtools/releases/download/1.4/samtools-1.4.tar.bz2
 BCFTOOLS_URL=https://github.com/samtools/bcftools/releases/download/1.4/bcftools-1.4.tar.bz2
